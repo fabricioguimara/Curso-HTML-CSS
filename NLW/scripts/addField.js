746807eda7f4f699936b4028f6697846fa05cc91
Fabricio <fabriciofraga21@example.com>
2020-08-07 11:58:42 -0300

    inicio do javascript no site

---
+++ NLW/scripts/addField.js
@@ -0,0 +1,13 @@
+// Procurar o botão 
+document.querySelector("#add-time")
+// Quando clicar no botao 
+.addEventListener('click',cloneField);
+// Executar uma ação 
+function cloneField() {
+    // Duplicar os campos
+    console.log("Cheguei aqui")
+    // Colocar na página
+}
+
+    
+
