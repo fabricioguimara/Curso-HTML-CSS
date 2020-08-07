ee37fc47d1de8629f91edf3b6acc34f6d5591097
Fabricio <fabriciofraga21@example.com>
2020-08-07 12:24:27 -0300

    fim do JS na give classes

--- NLW/scripts/addField.js
+++ NLW/scripts/addField.js
@@ -1,12 +1,20 @@
 // Procurar o botão 
 document.querySelector("#add-time")
 // Quando clicar no botao 
-.addEventListener('click',cloneField);
+.addEventListener('click',cloneField)
 // Executar uma ação 
 function cloneField() {
-    // Duplicar os campos
-    console.log("Cheguei aqui")
-    // Colocar na página
+    // Duplicar os campos. Que campos?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+    // pegar os campos. Que campos? 
+    const fields = newFieldContainer.querySelectorAll('input')
+    // para cada campo, limpar
+    fields.forEach(function(field){
+        // pega o field do momento e limpa ele
+        field.value = ""
+    })
+    // Colocar na página, onde?
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
 }
 
     
