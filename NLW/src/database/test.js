0e28fc37848f1edde333b51681d249543d89881a
Fabricio <fabriciofraga21@example.com>
2020-08-09 21:55:06 -0300

    final site proffy nlw

--- NLW/src/database/test.js
+++ NLW/src/database/test.js
@@ -12,7 +12,7 @@ Database.then(async(db) => {
     }
 
     classValue = {
-        subject: "Química",
+        subject: 1,
         cost: "30",
         // o proffy id vira pelo banco de dados
     }
@@ -31,7 +31,35 @@ Database.then(async(db) => {
         }
     ]
    
-    await createProffy(db, {proffyValue, classValue, classScheduleValues})
+    // await createProffy(db, {proffyValue, classValue, classScheduleValues})
 
     //consultar os dados inseridos
+
+    //todos os proffys
+    // const selectedProffys = await db.all("SELECT * FROM proffys")
+    // console.log(selectedProffys)
+
+    // consultar as classes de um determinado professor 
+    // e trazer junto os dados do professor 
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 2;
+    `)
+    console.log(selectClassesAndProffys)
+
+    // o horário que a pessoa trabalha é das 8h - 18h
+    // o horário do time_from (8h) precisa ser menor ou igual ao horario solicitado
+    // o time_to precisa ser acima
+    // const selectClassesSchedules = await db.all(`
+    //     SELECT class_schedule.* 
+    //     FROM class_schedule
+    //     WHERE class_schedule.class_id = 1
+    //     AND class_schedule.weekday = "0"
+    //     AND class_schedule.time_from <= "520"
+    //     AND class_schedule.time_to > "520";
+    // `)
+
+    // console.log(selectClassesSchedules)
 })
