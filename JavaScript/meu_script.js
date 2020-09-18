7ad7a15d3f594408e464a429b6deddaf9b05d3a6
Fabricio <fabriciofraga21@example.com>
2020-09-18 18:05:52 -0300

    Curso html-css JS até eventos mouse

--- JavaScript/meu_script.js
+++ JavaScript/meu_script.js
@@ -1,7 +1,7 @@
-// alert('Olá, seja bem vindo ao curso!!!')
-/*
-    selecionar um elemento no DOM
-    atualizar o valor desse elemento com uma string
-*/
+//alert('Olá, seja bem vindo ao curso!!!')
 
-document.getElementById("nome").value = 'Oi'
+/*
+	selecionar um elemento no DOM
+	atualizar o valor desse elemento com uma string
+*/
+document.getElementById('nome').value = 'Oi'
