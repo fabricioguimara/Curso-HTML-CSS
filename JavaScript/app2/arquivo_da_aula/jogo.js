992a28cb51ae60201d54ecd8a76be9ae149567aa
Fabricio <fabriciofraga21@example.com>
2020-09-24 03:00:18 -0300

    Curso html css JS jogo mosquito finalizado

--- JavaScript/app2/arquivo_da_aula/jogo.js
+++ JavaScript/app2/arquivo_da_aula/jogo.js
@@ -3,56 +3,63 @@ var altura = 0
 var largura = 0
 var vidas = 1
 var tempo = 15
+
 var criaMosquitoTempo = 1500
 
 var nivel = window.location.search
-nivel = nivel.replace('?','')
+nivel = nivel.replace('?', '')
 
-if (nivel === 'normal') {
+if(nivel === 'normal') {
+	//1500
 	criaMosquitoTempo = 1500
-} else if (nivel === 'dificil') {
+} else if(nivel === 'dificil') {
+	//1000
 	criaMosquitoTempo = 1000
-} else if (nivel === 'promaster') {
+} else if (nivel === 'chucknorris') {
+	//750
 	criaMosquitoTempo = 750
 }
-
 
 function ajustaTamanhoPalcoJogo() {
 	altura = window.innerHeight
 	largura = window.innerWidth
+
+	console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
 
 var cronometro = setInterval(function() {
-	tempo--
-	if (tempo < 0) {
-		document.getElementById('myImg').src = "imagens/vitoria.png"
-		resultado()
+
+	tempo -= 1
+
+	if(tempo < 0) {
 		clearInterval(cronometro)
-		clearInterval(criaMosquito)
+		clearInterval(criaMosca)
+		window.location.href = 'vitoria.html'
 	} else {
 		document.getElementById('cronometro').innerHTML = tempo
 	}
-},1000)
+	
+}, 1000)
+
 
 function posicaoRandomica() {
 
-	// remover o mosquito anterior (caso ele exista)
-	if (document.getElementById('mosquito')) {
+
+	//remover o mosquito anterior (caso exista)
+	if(document.getElementById('mosquito')) {
 		document.getElementById('mosquito').remove()
 
-		if (vidas > 3) {
-			clearInterval(cronometro)
-			clearInterval(criaMosquito)
-			resultado()
-			return false
+		//console.log('elemento selecionado foi: v' + vidas)
+		if(vidas > 3) {
+
+			window.location.href = 'fim_de_jogo.html'
 		} else {
 			document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+
 			vidas++
 		}
-
-
 	}
 
 	var posicaoX = Math.floor(Math.random() * largura) - 90
@@ -61,35 +68,49 @@ function posicaoRandomica() {
 	posicaoX = posicaoX < 0 ? 0 : posicaoX
 	posicaoY = posicaoY < 0 ? 0 : posicaoY
 
+	console.log(posicaoX, posicaoY)
 
 	//criar o elemento html
 	var mosquito = document.createElement('img')
-
 	mosquito.src = 'imagens/mosquito.png'
 	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 	mosquito.id = 'mosquito'
-	mosquito.onclick = function () {
+	mosquito.onclick = function() {
 		this.remove()
 	}
 
 	document.body.appendChild(mosquito)
+
 }
 
 function tamanhoAleatorio() {
-	// gera numero aleatorio entre 1 e 3
-	var classe = 'mosquito' + (Math.floor(Math.random() * 3) + 1)
-	return classe
+	var classe = Math.floor(Math.random() * 3)
+	
+	switch(classe) {
+		case 0:
+			return 'mosquito1'
+		
+		case 1:
+			return 'mosquito2'
+
+		case 2:
+			return 'mosquito3'
+	}
 }
 
 function ladoAleatorio() {
-	// gera numero aleatorio entre 1 e 2
-	var lado = 'lado' + (Math.floor(Math.random() * 2) + 1)
-	return lado
+	var classe = Math.floor(Math.random() * 2)
+	
+	switch(classe) {
+		case 0:
+			return 'ladoA'
+		
+		case 1:
+			return 'ladoB'
+
+	}
 }
 
-function resultado() {
-	document.getElementById("myDiv").className = 'showImage'
-}
